0113c6db3216e7c30292ce7c34587161c8bf0d74
knrd1 <36317223+knrd1@users.noreply.github.com>
2018-02-10 23:47:32 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # Script to bild new VM using OpenStack CLI tools
+# Syntax: build.sh <app> <environment> <num_servers> <server_size>
+
+source ~/Konrad-openrc.sh
+
+FLAVOR="m1.small"
+IMAGE="Ubuntu 16.04 LTS"
+KEY="konrad"
+SECGROUP="default"
+NIC="net-id=0a8d12af-e534-421a-8ad4-e326dc14dd9b"
+COUNT="1"
