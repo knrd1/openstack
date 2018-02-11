6686afc9f35478689b5a6476b6fd3184efed2118
knrd1 <36317223+knrd1@users.noreply.github.com>
2018-02-11 00:56:35 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 # Syntax: build.sh <app> <environment> <num_servers> <server_size>
 
 source ~/Konrad-openrc.sh
-
+VM="$1"
 FLAVOR="m1.small"
 IMAGE="Ubuntu 16.04 LTS"
 KEY="konrad"
-SECGROUP="default"
+GROUP="default"
 NIC="net-id=0a8d12af-e534-421a-8ad4-e326dc14dd9b"
 COUNT="1"
+    echo "Creating Virtual machine"
+        openstack server create --flavor "$FLAVOR" --image "$IMAGE" --key-name "$KEY" --security-group "$GROUP" --nic "$NIC" "$1"
+    echo "Virtual Machine created"
