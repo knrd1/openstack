6f2051b8564dd1d33a2ff8e5ab7676132f757319
knrd1 <36317223+knrd1@users.noreply.github.com>
2018-02-12 00:45:33 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,66 +1,57 @@
 #!/bin/bash
-# Script to build new VM using OpenStack CLI tools
-# Syntax: build.sh <app> <num_servers> <server_size>
+# Script to bild new VM using OpenStack CLI tools
+# Syntax: build.sh <server name> <number of servers> <server size>
 
-if [ $# -eq 0 -o $# -gt 4 ]; then
-   echo "Syntax: ./build.sh <VM name> <number of servers> <server size>" 1>&2
-   exit 1
+if [ $# -eq 0 -o $# -gt 3 ]; then
+  echo "Syntax: ./build.sh <server name> <number of servers> <server size>" 1>&2
+  echo " "
+  exit 1
 fi
+
 PS3="Choose option: "
 
-select menu in create leave
+select menu in create leave; do
+  if [ "$menu" = "" ] ; then
+    echo "Please choose option"
+    echo " "
+    continue
+  elif [ "$menu" = leave ] ; then
+    echo "Bye"
+    echo " "
+    break
+  elif [ "$menu" = create ] ; then
+    read -p "Do you want to create new virtual machine? y/n: " choice
+    echo " "
+      if [ "$choice" = n -o "$choice" = "" ] ; then
+         continue
+      elif [ "$choice" = y ] ; then
 
-do
+        source ~/Konrad-openrc.sh
+        VM="$1"
+        COUNT="$2"
+        FLAVOR="$3"
+        IMAGE="Ubuntu 16.04 LTS"
+        KEY="konrad"
+        GROUP="default"
+        NIC="net-id=0a8d12af-e534-421a-8ad4-e326dc14dd9b"
+        USERDATA="userdata.txt"
 
-if [ "$menu" = "" ] ; then
-        echo "Please choose option"
         echo " "
-        continue
-
-elif [ "$menu" = leave ] ; then
-        echo "Bye"
-        echo " "
-        break
-
-elif [ "$menu" = create ] ; then
-        read -p "Do you want ot create new virtual machine? y/n: " choice
+        echo "Creating Virtual machine..."
         echo " "
 
-        if [ "$choice" = n ] ; then
-                continue
-        elif [ "$choice" = y ] ; then
+        echo " "
+        echo "Virtual Machine has been created, please associate floating IP in GUI and press ENTER to continue."
+        read
+        echo " "
 
-source ~/Konrad-openrc.sh
-VM="$1"
-COUNT="$2"
-FLAVOR="$3"
-IMAGE="Ubuntu 16.04 LTS"
-KEY="konrad"
-GROUP="default"
-NIC="net-id=0a8d12af-e534-421a-8ad4-e326dc14dd9b"
-USERDATA="userdata.txt"
+     fi
+  fi
 
-   echo "Creating Virtual machine"
-        openstack server create \
-        --flavor "$FLAVOR" \
-        --image  "$IMAGE" \
-        --key-name "$KEY" \
-        --security-group "$GROUP" \
-        --nic "$NIC" \
-        --user-data "$USERDATA" \
-        "$VM"
+  while true; do
+  echo "Please wait...";
+  /usr/bin/wget "http://87.254.4.135" --timeout 6 -O - 2>/dev/null | grep "Hello";
+  sleep 10;
+  done
 
-   echo "Virtual Machine created"
-      echo "Virtual Machine created, please associate floating IP "87.254.4.135" in GUI and press enter to continue."
-   read
-   
-        continue
-        fi
-fi
-
-while true; do
-echo "Please wait...";
-/usr/bin/wget "http://87.254.4.135" --timeout 6 -O - 2>/dev/null | grep "Hello";
-sleep 10;
-done
 done
